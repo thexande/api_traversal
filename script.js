c6e302fb23efa27cb5d8298b0e50e356b31600cf
Alex <amurphy345@gmail.com>
2016-06-08 10:07:37 -0600

    media query and api call

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $('#search').click(function() {
+    var search = $('#text_field').val()
+    $.getJSON('http://www.omdbapi.com/?s=' + search + '', function(data) {
+      $('#center_column').html("")
+      data.Search.forEach(function(val, key) {
+        var $title = $("<div>", {html: val.Title + ", " + val.Year})
+        if(val.Poster === "N/A"){
+          var $poster = $("<img>", {class:'movie_poster', src:"mockups/images/no_image.png"})
+        } else {
+          var $poster = $("<img>", {class:'movie_poster', src:val.Poster})
+        }
+        $('#center_column').append($title, $poster)
+      })
+    })
+  })
+})
